cc5cfaa1eb604d0ef29c52ed9be8647ec491936e
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-02-14 11:42:40 +0545

    index

--- index.js
+++ index.js
@@ -3,23 +3,29 @@ const cors = require("cors");
 const connectDb = require("./config/db");
 const AuthRouter = require("./routes/authRoutes");
 const protectedRouter = require("./routes/protectedRoutes");
+const roomRoutes = require("./routes/roomRoutes");
 
 const app = express();
 
 connectDb();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json());
 
+// Route handling
 app.use("/api/auth", AuthRouter);
 app.use("/api/protected", protectedRouter);
+app.use("/api/rooms", roomRoutes);  // Add room routes here
+
+// Serve static files from the "uploads" directory
+app.use("/uploads", express.static("uploads"));
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
