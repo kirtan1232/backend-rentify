ef09ab06d951ac63c50fb080771ad9581f6627b7
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-02-21 11:02:00 +0545

    index

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const connectDb = require("./config/db");
 const AuthRouter = require("./routes/authRoutes");
+const MobileRouter = require("./routes/mobileRoutes");
 const protectedRouter = require("./routes/protectedRoutes");
 const roomRoutes = require("./routes/roomRoutes");
 const userRoutes = require("./routes/userRoutes");
 const wishlistRoutes = require("./routes/wishlistRoutes");
+const emailRoutes = require('./routes/emailRoutes');
+const contactRoutes = require('./routes/contactRoutes');
+
+require('dotenv').config();
 
 
 const app = express();
@@ -22,10 +27,13 @@ app.use(express.json());
 
 // Route handling
 app.use("/api/auth", AuthRouter);
+app.use("/api/v1/", MobileRouter);
 app.use("/api/protected", protectedRouter);
 app.use("/api/rooms", roomRoutes);  // Add room routes here
 app.use("/api/user", userRoutes);
 app.use('/api', wishlistRoutes);
+app.use('/api/email', emailRoutes);
+app.use('/api', contactRoutes);
 app.use("/uploads", express.static("uploads"));
 
 const port = 3000;
